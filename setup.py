00e6c5dcce7a5793c1b9663bc1d95c3a3ee8fe5e
a.sviridov <sviridov.ao@yandex.ru>
2017-02-20 17:35:25 +0300

    initial setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='boxcomtools',
+      version='0.0.1',
+      description='A set of tools to stream data between cloud providers (box.com, smartsheet)',
+      author='Andrey Sviridov',
+      author_email='isgondurasa@gmail.com',
+      url='',
+      py_modules=['boxsdk'],
+      packages=['boxcomtools'],
+     )
