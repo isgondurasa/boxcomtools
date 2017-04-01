b774d1ce244393302a005232bb71d1fbcdccd5a3
a.sviridov <sviridov.ao@yandex.ru>
2017-04-01 23:45:24 +0300

    transfer_client

---
+++ boxcomtools/base/transfer_client.py
@@ -0,0 +1,29 @@
+import asyncio
+
+
+class TransferClient:
+    """
+    Transfer data endpoint
+    assume that both sourse and destination 
+    as access and refresh tokens already
+    """
+    
+    def __init(self, source, destination):
+        self._source = source
+        self._destination = destination
+
+
+class BoxToSmartsheet(TransferClient):
+    async def transfer_metadata(self):
+        folder = self._source.folder
+        
+        all_metadata = []
+        for _file in  await folder.files:
+            all_metadata.append(_file.get_metadata())
+    
+        all_metadata = await asyncio.gather(*all_metadata)
+        
+        
+class SmartsheetToBox(TransferClient):
+    async def transfer(self):
+        raise NotImplementedError
