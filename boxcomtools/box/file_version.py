1db7ec4046bd50bdc4d42fdfdb70222d703ba84c
isgondurasa <sviridov.ao@yandex.ru>
2017-06-01 08:24:16 +0300

    refactor & pep8

--- boxcomtools/box/file_version.py
+++ boxcomtools/box/file_version.py
@@ -1,21 +1,15 @@
 # file_version.py
 
 from boxcomtools.base.base_object import BaseObject
+from boxcomtools.base.config import BoxConfig
 
 
 class FileVersion(BaseObject, BoxConfig):
     """
-    File version API endpoint   
+    File version API endpoint
     """
     __resource__ = "versions"
 
-
     def get_url(self):
-        url = self.request_url + 'files/%s/%s' % (self.id,
-                                                  self.__resource__)
-
-    async def get(self):
-        """
-        curl https://api.box.com/2.0/files/FILE_ID/versions \
-        -H "Authorization: Bearer ACCESS_TOKEN"
-        """
+        return self.request_url + 'files/%s/%s' % (self.id,
+                                                   self.__resource__)
