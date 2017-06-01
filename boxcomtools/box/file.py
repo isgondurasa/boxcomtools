9ad3971d416cf16bd22ded2bd692c2b7a6628e39
isgondurasa <sviridov.ao@yandex.ru>
2017-06-01 11:32:30 +0300

    docker added

--- boxcomtools/box/file.py
+++ boxcomtools/box/file.py
@@ -13,13 +13,13 @@ class File(BaseObject, BoxConfig):
     __resource__ = "files"
 
     def __init__(self, session, object_id=None, name=""):
-        super(File, self).__init__(session, object_id)        
+        super(File, self).__init__(session, object_id)
         self._metadata = Metadata(session, object_id)
         self._versions = FileVersion(session, object_id)
         self._cached_metadata = None
         self._name = name
         self._data = None
-        
+
     async def get(self):
         """
         curl https://api.box.com/2.0/files/FILE_ID
@@ -36,7 +36,7 @@ class File(BaseObject, BoxConfig):
     async def get_versions(self):
         versions = await self._versions.get()
         return versions['entries']
-    
+
     def to_dict(self):
         if not self._data:
             return {}
